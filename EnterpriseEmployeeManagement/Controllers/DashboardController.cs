584a43d53fd27f340d19e46ddb7c9c11a9f6c82d
vandy11 <vandy11@users.noreply.github.com>
2026-03-10 11:08:47 +0700

    Add dashboard with employee and department statistics

--- EnterpriseEmployeeManagement/Controllers/DashboardController.cs
+++ EnterpriseEmployeeManagement/Controllers/DashboardController.cs
@@ -1,11 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EnterpriseEmployeeManagement.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EnterpriseEmployeeManagement.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public DashboardController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            var totalEmployees = _context.Employees.Count();
+            var totalDepartments = _context.Departments.Count();
+
+            ViewBag.TotalEmployees = totalEmployees;
+            ViewBag.TotalDepartments = totalDepartments;
+
             return View();
         }
     }
